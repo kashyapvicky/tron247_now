abca3807978c1a7f99ee240c0bc5b384fb870c92
vicky <veee.kay258@gmail.com>
2020-12-27 03:08:11 +0530

    initial commit

---
+++ precache-manifest.f2dc876bb099348106b84101af8e44ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c55f491ce9da9f557f58",
+    "url": "/static/css/main.87e1b8bb.chunk.css"
+  },
+  {
+    "revision": "c55f491ce9da9f557f58",
+    "url": "/static/js/main.2186cca8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "277434fddd7d1307938e",
+    "url": "/static/js/2.844a6f97.chunk.js"
+  },
+  {
+    "revision": "644a6b13d191bd34e4384b699dba495a",
+    "url": "/index.html"
+  }
+];
